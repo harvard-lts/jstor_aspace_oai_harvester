d4c24caecaebf449b7e044d25f35ca7bad18896f
Michael Vandermillen <michael_vandermillen@harvard.edu>
2022-11-18 15:00:54 -0500

    prototyping jstor oai harvest

--- tests/invoke-task.py
+++ tests/invoke-task.py
@@ -4,4 +4,4 @@ app1 = Celery('tasks')
 app1.config_from_object('celeryconfig')
 
 # Send a simple task (create and send in 1 step)
-res = app1.send_task('tasks.tasks.do_task', args=[{"job_ticket_id":"123","hello":"world"}], kwargs={}, queue="harvest_jstorforum")
+res = app1.send_task('tasks.tasks.do_task', args=[{"job_ticket_id":"123","hello":"world"}], kwargs={}, queue="proto_harvest")
