c3ad775f8362a6b7aee322102d357bebc8b609a2
Michael Vandermillen <michael_vandermillen@harvard.edu>
2023-04-25 11:29:30 -0400

    restore 'last successful' test, add records to test date for restore

--- tests/test_mongo.py
+++ tests/test_mongo.py
@@ -7,9 +7,8 @@ import pytest
 load_dotenv()
 
 harvestdate = date.today() - timedelta(days = 1)
-successdate = datetime(2023, 3, 7)
+successdate = datetime(2023, 4, 22)
 
-@pytest.mark.skip(reason="test collection in mongo pending")
 def test_success_date():
 
     mongo_url = os.environ.get('MONGO_URL')
@@ -17,7 +16,7 @@ def test_success_date():
     mongo_client = MongoClient(mongo_url, maxPoolSize=1)
 
     mongo_db = mongo_client[mongo_dbname]
-    testcoll = mongo_db['jstor_harvester_summary_test']
+    testcoll = mongo_db['test_jstor_harvested_summary']
     rec = testcoll.find({"repository_id":"713", "success":True}, {"harvest_date":1}).sort("harvest_date", -1).limit(1)
     latest_mongo_success_date = rec[0]['harvest_date']
 
